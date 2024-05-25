f3df99a1b5257296cb5bd45506cde410c701b71b
이온유 <ion-yu@ion-yuui-MacBookPro.local>
2024-05-25 09:26:58 +0900

    온유: 에러 해결

--- meojeori/src/Pages/Mypage/Mypage.js
+++ meojeori/src/Pages/Mypage/Mypage.js
@@ -1,24 +1,40 @@
-import react from "react";
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import fire from './fire.png';
 import coin from './coin.png';
 import offfire from './offfire.png'
 import Back from '../../Assets/Back.svg';
+import { getUserData } from '../../AxiosAPI';
+import Header from '../Common/Header';
 
 function Mypage() {
+    const [userData, setUserData] = useState({}); // 유저 데이터를 저장할 state 추가
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await getUserData("userId"); 
+                setUserData(response.data); // 가져온 데이터를 state에 저장
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+        fetchData();
+    }, []); // 마운트될 때 한 번만 실행
+
     return (
         <div>
-            <Container Width="100vw" Height="100vh" Image={`url(${Back})`}>
+            <Container MarginTop="800px" Width="100vw" Height="2090px" Image={`url(${Back})`} >
+            <Container MarginTop="20px"><Header /></Container>
             <Container Height="220px" MarginTop="30px" Color="transparent">
                 <Circle Width="160px" Height="160px"></Circle>
             </Container>
             
-            <Container Color="transparent"><Container Border="2px solid black" Width="396px" Height="110px" Color="white" Shadow="5px 5px 0px 0px rgba(0, 0, 0, 1)">
+            <Container Color="transparent" MarginTop="-30px"><Container Border="2px solid black" Width="396px" Height="110px" Color="white" Shadow="5px 5px 0px 0px rgba(0, 0, 0, 1)">
                 <Container Width="10px"><Circle Width="8px" Height="8px" Color="black"></Circle></Container>
                 <Container Width="360px"></Container>
                 <Container Width="10px"><Circle Width="8px" Height="8px" Color="black"></Circle></Container>
-                <Container Width="90%" Justify="left"><Text fontSize="17px" MarginLeft="20px">NAME: 구글 로그인 이름</Text></Container>
-                <Container Width="90%" Justify="left"><Text fontSize="17px" MarginLeft="20px">E-MAIL: google@gamil.com</Text></Container>
+                <Container Width="90%" Justify="left"><Text fontSize="17px" MarginLeft="20px">NAME: {userData.name || ''}</Text></Container>
+                <Container Width="90%" Justify="left"><Text fontSize="17px" MarginLeft="20px">E-MAIL: {userData.email || ''}</Text></Container>
                 <Container>
 
                 <Container Width="10px"><Circle Width="8px" Height="8px" Color="black"></Circle></Container>
@@ -26,14 +42,15 @@ function Mypage() {
                 <Container Width="10px"><Circle Width="8px" Height="8px" Color="black"></Circle></Container>
                 </Container>
                 </Container></Container>
+
             <Container Width="805px" Color="transparent">
 
             
-            <Container Border="2px solid black" Width="805px" Height="713px" Align="top" MarginTop="70px" Shadow="5px 5px 0px 0px rgba(0, 0, 0, 1);">
-                <Container Background="linear-gradient(to bottom, #EDFFD8, #9DE44D)" Color="#D9D9D9" Height="713px" Direction="column" Justify="top">
+            <Container Border="2px solid black" Width="805px" Height="630px" Align="top" MarginTop="-30px" Shadow="5px 5px 0px 0px rgba(0, 0, 0, 1);">
+                <Container Background="linear-gradient(to bottom, #EDFFD8, #9DE44D)" Color="#D9D9D9" Height="630px" Direction="column" Justify="top">
                     <Container MarginTop="50px" Width="740px" Height="50px" Justify="left" Color="transparent">
                         <Circle Width="36px" Height="36px" />
-                        <Text fontSize="20px" MarginLeft="10px">유저 네임</Text>
+                        <Text fontSize="20px" MarginLeft="10px">{userData.name || ''}</Text>
                     </Container>
                     <Container Color="#E6E6E6" Width="740px" Height="366px" Align="top"></Container>
                     <Container Width="740px" Height="50px" Color="transparent">
@@ -41,12 +58,12 @@ function Mypage() {
                             <img src={fire} alt="이미지 설명" />
                         </Container>
                         <Container Justify="left" Width="680px" Color="transparent">
-                            <Text fontSize="17px" FontWeight="bold" MarginLeft="0px">숫자 낭만 지수</Text>
+                            <Text fontSize="17px" FontWeight="bold" MarginLeft="0px">{userData.id || ''} 낭만 지수</Text>
                         </Container>
                     </Container>
                     <Container Width="740px" Height="50px" Color="transparent">
                         <Container Justify="left" Width="570px" MarginLeft="30px" Color="transparent">
-                            <Text fontSize="22px" FontWeight="bold" MarginLeft="10px">제목</Text>
+                            <Text fontSize="22px" FontWeight="bold" MarginLeft="10px">{userData.introduce || ''}</Text>
                         </Container>
                         <Container Justify="right" Width="90px" Color="transparent">
                             <Text fontSize="17px">가격</Text>
@@ -60,11 +77,11 @@ function Mypage() {
                     </Container>
                 </Container>
             </Container>
-            </Container>
+            
 
-            <Container Width="100vw" Height="100vh" Image={`url(${Back})`} MarginTop="10px" Align="Top">
-            <Container Border="2px solid black" Width="805px" Height="713px" Align="top" MarginTop="70px" Shadow="5px 5px 0px 0px rgba(0, 0, 0, 1);">
-                <Container Color="white" Height="713px" Direction="column" Justify="top">
+
+            <Container Border="2px solid black" Width="805px" Height="630px" Align="top" MarginTop="70px" Shadow="5px 5px 0px 0px rgba(0, 0, 0, 1);">
+                <Container Color="white" Height="630px" Direction="column" Justify="top">
                     <Container MarginTop="50px" Width="740px" Height="50px" Justify="left" Color="transparent">
                         <Circle Width="36px" Height="36px" />
                         <Text fontSize="20px" MarginLeft="10px">유저 네임</Text>
@@ -95,7 +112,6 @@ function Mypage() {
                 </Container>
             </Container>
             </Container>
-
             </Container>
 
 
@@ -118,6 +134,7 @@ export const Container = styled.div`
     flex-direction: ${(props) => props.Direction || 'none'};; /* 세로 방향으로 아이템 나열 */    
     flex-wrap: wrap;
     box-shadow: ${(props) => props.Shadow || 'none'};
+    
 `;
 
 export const Circle = styled.div`
